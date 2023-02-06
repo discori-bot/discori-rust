41d568b7cce9df6b73732974015ae97f679ffd0c
Cashew <tungbinem@gmail.com>
2023-02-07 01:15:41 +0700

    refactor: switch to ? operator

--- src/commands/invite.rs
+++ src/commands/invite.rs
@@ -9,8 +9,6 @@ use serenity::prelude::*;
 async fn invite(ctx: &Context, msg: &Message) -> CommandResult {
     let invite = env::var("INVITE_LINK").expect("Expected an invite link in the environment");
 
-    if let Err(e) = msg.channel_id.say(&ctx.http, format!("Here's my invite link: \n {}", invite)).await {
-        eprintln!("Sending message failed: {:?}", e);
-    }
+    msg.channel_id.say(&ctx.http, format!("Here's my invite link: \n {}", invite)).await?;
     Ok(())
 }
